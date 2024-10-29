6f4b76ea4eb31add60c0229fec7d9def91b853a3
myathtut-zafir <aa.htut@gmail.com>
2024-10-29 23:27:31 +0700

    feat: sample project to linkedin url scrapting

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,26 +4,29 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
+from third_parties.linkedin import scrape_linkedin_profile
 
 if __name__ == "__main__":
-    information = """Musk was born in Pretoria, South Africa, to Maye (née Haldeman), a model, and Errol Musk, a businessman and engineer. Musk briefly attended the University of Pretoria before immigrating to Canada at the age of 18, acquiring citizenship through his Canadian-born mother. Two years later he matriculated at Queen's University at Kingston in Canada. Musk later transferred to the University of Pennsylvania and received bachelor's degrees in economics and physics. He moved to California in 1995 to attend Stanford University, but dropped out after two days and, with his brother Kimbal, co-founded the online city guide software company Zip2. The startup was acquired by Compaq for $307 million in 1999. That same year Musk co-founded X.com, a direct bank. X.com merged with Confinity in 2000 to form PayPal. In 2002 Musk acquired US citizenship. That October eBay acquired PayPal for $1.5 billion. Using $100 million of the money he made from the sale of PayPal, Musk founded SpaceX, a spaceflight services company, in 2002.
+    information = """
+    
     """
 
     load_dotenv()
     print("hello langchain")
     # print(os.environ["OPENAI_API_KEY"])
     summary_template = """
-    give the information {information} I want to create.
+    give the linkedin information {information} I want to create.
     1. a short summary
     2. two interesting fact.
     """
     summary_prompt_template = PromptTemplate(
         input_variables=["information"], template=summary_template
     )
-    # llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    llm = ChatOllama(model="mistral")
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    # llm = ChatOllama(model="mistral")
     # llm = ChatOllama(model="llama3")
+    linked_data=scrape_linkedin_profile(linked_url="https://www.linkedin.com/in/myat-htut/",mock=True)
     chain = summary_prompt_template | llm|StrOutputParser()
-    res = chain.invoke(input={"information": information})
+    res = chain.invoke(input={"information": linked_data})
 
     print(res)
