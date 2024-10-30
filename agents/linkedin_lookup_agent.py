f39704e4367186cb1865937f609a175bd2a8f753
myathtut-zafir <aa.htut@gmail.com>
2024-10-30 12:00:00 +0700

    feat: setup agent

---
+++ agents/linkedin_lookup_agent.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.tools import Tool
+from langchain.agents import (create_react_agent,AgentExecutor)
+from langchain import hub
+
+load_dotenv()
+def lookup(name:str)->str:
+    llm=ChatOpenAI(temperature=0,model_name="gpt-4o-mini")
+    return "https://www.linkedin.com/in/myat-htut/"
